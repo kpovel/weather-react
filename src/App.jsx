91e1b9863c92b789bf13c7b8d0d7a5820324e9cb
kpovel <kar.pavel123@gmail.com>
2022-07-09 21:16:01 +0300

    Added save input value on state when entering a value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import {SearchForm} from "./components/searchForm";
 import {MainDisplay} from "./components/mainDisplay";
+import {useState} from "react";
 
 function App() {
+    const [location, setLocation] = useState("");
+    
+    function saveLocationName(e) {
+        setLocation(e);
+    }
+    
+    function handleSubmit() {
+    //    todo: weather request from the server
+    }
+    
     return (
         <div>
             <main className="main">
                 <div className="container">
-                    <SearchForm/>
+                    <SearchForm onHandleChange={saveLocationName}
+                                value={location}
+                                onHandleSubmit={handleSubmit}/>
                     <MainDisplay/>
                 </div>
             </main>
--- src/components/searchForm.jsx
+++ src/components/searchForm.jsx
@@ -1,11 +1,20 @@
 export {SearchForm};
 
-function SearchForm() {
+function SearchForm(props) {
+    function handleChange(e) {
+        props.onHandleChange(e.target.value);
+    }
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.onHandleSubmit();
+    }
+    
     return (
-        <div className="search-form">
-            <input className="search__input"
-                type="text" autoComplete="off" placeholder="Type the city" id="input"/>
+        <form className="search-form" onSubmit={handleSubmit}>
+            <input className="search__input" type="text" autoComplete="off" placeholder="Type the city"
+                   onChange={handleChange} value={props.value}/>
             <button className="search__btn" type="submit"/>
-        </div>
+        </form>
     );
 }
