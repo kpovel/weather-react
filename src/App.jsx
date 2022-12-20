1c5cbcba43e916804578429553fc0fac611343f2
Pavlo Karas <kar.pavel123@gmail.com>
2022-12-21 00:05:58 +0200

    Memorize MainDisplay components and a few functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,73 @@
 import "./App.css";
 import {SearchForm} from "./components/searchForm";
 import {MainDisplay} from "./components/mainDisplay";
-import {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useCallback, useState} from "react";
+import {useDispatch} from "react-redux";
 import {setForecastWeather, setWeatherNow} from "./store/action/action";
 
 function App() {
-    const weatherData = useSelector(state => state.setWeather);
-    const weatherForecastData = useSelector(state => state.setWeatherForecastData);
     const dispatch = useDispatch();
-    
     const [location, setLocation] = useState("");
     const API_KEY = "4783b73cfe02019303d03a9d793cc64b";
-    
-    function saveLocationName(e) {
-        setLocation(e);
-    }
-    
-    function handleSubmit() {
-        getWeather(location);
-        getForecastWeather(location);
-        setLocation("");
-    }
-    
-    function searchWeatherFromSavedList(city) {
-        getWeather(city);
-        getForecastWeather(city);
-    }
-    
-    async function getWeather(city) {
+
+    const getWeather = useCallback(async (city) => {
         const SERVER_URL = "https://api.openweathermap.org/data/2.5/weather";
         const url = `${SERVER_URL}?q=${city}&appid=${API_KEY}`;
-        
+
         try {
             const response = await fetch(url);
             const weather = await response.json();
-            
+
             const receivedError = !response.ok;
             if (receivedError) {
                 throw new Error(weather.message);
             }
-            
+
             dispatch(setWeatherNow(weather));
             localStorage.setItem("lastSearchedCity", JSON.stringify(weather));
         }
         catch (err) {
             alert(err);
         }
-    }
-    
-    async function getForecastWeather(city) {
+    }, [API_KEY, dispatch]);
+
+    const getForecastWeather = useCallback(async function (city) {
         const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
         try {
             const url = `${FORECAST_URL}?q=${city}&cnt=3&appid=${API_KEY}`;
             const response = await fetch(url);
             const forecastWeather = await response.json();
-            
+
             dispatch(setForecastWeather(forecastWeather));
             localStorage.setItem("lastSearchedForecast", JSON.stringify(forecastWeather));
         }
         catch (err) {
             alert(err);
         }
+    }, [API_KEY, dispatch]);
+
+    function saveLocationName(e) {
+        setLocation(e);
     }
-    
+
+    const handleSubmit = useCallback(async () => {
+        await getWeather(location);
+        await getForecastWeather(location);
+        setLocation("");
+    }, [getForecastWeather, getWeather, location]);
+
+    const searchWeatherFromSavedList = useCallback(async (city) => {
+        await getWeather(city);
+        await getForecastWeather(city);
+    }, [getForecastWeather, getWeather]);
+
     return (
         <main className="main">
             <div className="container">
                 <SearchForm onHandleChange={saveLocationName}
                             value={location}
                             onHandleSubmit={handleSubmit}/>
-                <MainDisplay weatherNow={weatherData}
-                             forecastWeather={weatherForecastData}
-                             searchCity={searchWeatherFromSavedList}/>
+                <MainDisplay searchCity={searchWeatherFromSavedList}/>
             </div>
         </main>
     );
