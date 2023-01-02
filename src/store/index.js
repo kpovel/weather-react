af2e7a7256472fb6342adce3613d121b5fbebb8d
Pavlo Karas <kar.pavel123@gmail.com>
2023-01-02 17:51:07 +0200

    Configured Redux store with using RTK

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
 import {createLogger} from "redux-logger";
 import weather from "./reducers";
-import {applyMiddleware, compose, createStore} from "redux";
 
 const loggerMiddleware = createLogger();
 
-const middlewareEnhancer = applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-);
-
-const composedEnhancers = compose(
-    middlewareEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-export const store = createStore(
-    weather,
-    composedEnhancers
-);
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-unsubscribe();
+export const store = configureStore({
+    reducer: weather,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(thunkMiddleware, loggerMiddleware)
+});
